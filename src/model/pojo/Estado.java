41cef6291679f2ed9e0299229ee6875339967cf4
Jpvmello <jpvmello@hotmail.com>
2017-04-16 17:31:52 -0300

    Primeiro commit do projeto

---
+++ src/model/pojo/Estado.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.pojo;
+
+/**
+ *
+ * @author JeanPablo
+ */
+public class Estado {
+    private final String codigoEstado;
+    private final String estado;
+    
+    public Estado (String codigoEstado, String estado) {
+        this.codigoEstado = codigoEstado;
+        this.estado = estado;
+    }
+}
